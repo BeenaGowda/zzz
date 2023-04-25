8b5d2696b0960f513097b5a796afed183769a796
BeenaGowda <beenagowda54@gmail.com>
2023-04-25 22:27:01 +0530

    7th commit

--- src/test/java/zx11/zzz.java
+++ src/test/java/zx11/zzz.java
@@ -1,11 +1,36 @@
 package zx11;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
 public class zzz {
 
+	@Test
 	public void m1()
 	{
-		System.out.println("aaa");
-		//pushhhh
-		//pushhh1
+		WebDriver driver=null;
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println(BROWSER);
+		
+		if(BROWSER.equalsIgnoreCase("browser"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}
+		else if(BROWSER.equalsIgnoreCase("firefox"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver=new FirefoxDriver();
+		}
+		
+		driver.get(URL);
+		//push jenkins
 	}
 }
